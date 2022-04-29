77483e340457507d5d5b22bd8b025cb2649a35de
rzldev <amrizalrf@gmail.com>
2022-04-30 01:08:06 +0700

    Django Rest API

---
+++ S8-DjangoRestAPI/django_project/django_app/serializers.py
@@ -0,0 +1,31 @@
+from unittest.util import _MAX_LENGTH
+from rest_framework import serializers
+from django_app.models import Article
+
+
+# Serializer
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=100)
+#     author = serializers.CharField(max_length=255)
+#     email = serializers.EmailField()
+#     date = serializers.DateField()
+
+#     def create(self, validated_data):
+#         return Article.objects.create(validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.email = validated_data.get('email', instance.email)
+#         instance.date = validated_data.get('date', instance.date)
+#         instance.save()
+
+#         return instance
+
+
+# ModelSerializer
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Article
+        # fields = ['id', 'title', 'author', 'email']
+        fields = '__all__'
